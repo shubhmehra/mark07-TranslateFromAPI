890d80fbb7ad310b530b181e110ff687b65d9298
Shubham Mehra <mehrashubham18@gmail.com>
2021-08-08 00:20:13 +0530

    Logic & UI update

---
+++ app.js
@@ -0,0 +1,33 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var errorDiv = document.querySelector("#errorMessage");
+
+var serverUrl = "https://api.funtranslations.com/translate/fudd.json";
+
+// formatting url
+function getTranslationUrl(text) {
+  return serverUrl + "?" + "text=" + text;
+}
+
+// error handling function
+function errorHandler(error) {
+  console.log(" error occured", error);
+  errorDiv.innerText =
+    "We are sorry. You can only use this for 5 times per hour. Try again later.";
+}
+
+// click event
+btnTranslate.addEventListener("click", clickEventHandler());
+
+// handling click event
+function clickEventHandler() {
+  return function clickEventHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationUrl(inputText))
+      .then((response) => response.json())
+      .then((json) => (outputDiv.innerText = json.contents.translated))
+      .catch(errorHandler);
+  };
+}
